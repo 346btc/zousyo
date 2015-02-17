c2fdec6b681b96aa0d7072a96fd50bae397d8162
nakayay <nakayay@funai.co.jp>
2015-02-17 19:18:52 +0900

    GUIの初めのUpdate

--- project/app/src/main/java/jp/co/funai/zousyo/MainActivity.java
+++ project/app/src/main/java/jp/co/funai/zousyo/MainActivity.java
@@ -4,14 +4,31 @@ import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.Toast;
+import android.widget.ViewFlipper;
+
+import java.util.Calendar;
 
 
 public class MainActivity extends ActionBarActivity {
+    ViewFlipper viewflipper;
+    Animation in_from_left;
+    Animation out_to_left;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        viewflipper = (ViewFlipper)findViewById(R.id.viewFlipper1);
+        in_from_left = AnimationUtils.loadAnimation(this, R.anim.in_from_left);
+        out_to_left = AnimationUtils.loadAnimation(this, R.anim.out_to_left);
+        viewflipper.setInAnimation(in_from_left);
+        viewflipper.setOutAnimation(out_to_left);
     }
 
 
@@ -35,5 +52,71 @@ public class MainActivity extends ActionBarActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    public void onClick_Button1(View view) {
+        RadioGroup radiogroup = (RadioGroup)findViewById(R.id.radiogroup);
+        RadioButton radiobutton = (RadioButton)findViewById(radiogroup.getCheckedRadioButtonId());
+        if (radiobutton != null) {
+            Toast.makeText(this, radiobutton.getText(), Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    public void onClick_Button2(View view) {
+        RadioGroup radiogroup = (RadioGroup)findViewById(R.id.radiogroup);
+        RadioButton radiobutton = (RadioButton)findViewById(radiogroup.getCheckedRadioButtonId());
+        if (radiobutton != null) {
+            radiobutton.setChecked(false);
+        }
+        Calendar calendar = Calendar.getInstance();
+        long time = calendar.getTimeInMillis();
+        RadioButton newradio = new RadioButton(this);
+        newradio.setText(String.valueOf(time));
+        radiogroup.addView(newradio);
+
+        viewflipper.showNext();
+/*
+		RelativeLayout layout = (RelativeLayout)findViewById(R.id.relativeLayout1);
+		ObjectAnimator animation = ObjectAnimator.ofFloat(layout, "x", 100);
+		animation.setStartDelay(1000);
+		animation.start();
+		*/
+    }
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        RadioGroup radiogroup = (RadioGroup)findViewById(R.id.radiogroup);
+        boolean ischecked = false;
+        int checked_id = -1;
+        int cnt = radiogroup.getChildCount();
+        outState.putInt("radiobutton_cnt", cnt);
+        for (int i = 0; i < cnt; i++) {
+            RadioButton radiobutton = (RadioButton)radiogroup.getChildAt(i);
+            outState.putString("radiobutton" + i, radiobutton.getText().toString());
+            if (radiobutton.isChecked()) {
+                ischecked = true;
+                checked_id = i;
+            }
+        }
+        outState.putBoolean("radiobutton_checked", ischecked);
+        if (ischecked) {
+            outState.putInt("radiobutton_check_id", checked_id);
+        }
+    }
+    @Override
+    protected void onRestoreInstanceState(Bundle savedinstanceState) {
+        super.onRestoreInstanceState(savedinstanceState);
+        RadioGroup radiogroup = (RadioGroup)findViewById(R.id.radiogroup);
+        int cnt = savedinstanceState.getInt("radiobutton_cnt");
+        for (int i = 0; i < cnt; i++) {
+            RadioButton newradio = new RadioButton(this);
+            newradio.setText(savedinstanceState.getString("radiobutton" + i));
+            radiogroup.addView(newradio);
+        }
+        if (savedinstanceState.getBoolean("radiobutton_checked")) {
+            RadioButton radiobutton = (RadioButton)radiogroup.getChildAt(
+                    savedinstanceState.getInt("radiobutton_check_id"));
+            radiobutton.setChecked(true);
+        }
     }
 }
